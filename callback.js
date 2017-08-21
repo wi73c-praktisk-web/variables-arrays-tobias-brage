d3f63b0fbc376555b6f682fe829054bbe2bbd1ac
Tobias Brage <gamerfreaq@live.com>
2017-08-21 11:02:15 +0200

    callback

---
+++ callback.js
@@ -0,0 +1,25 @@
+// function doSomething(fName, callback){
+//     callback(fName);
+//  }
+
+//  function outputSomething(fname){
+//     console.log(fname);
+//  }
+
+//  doSomething('Tobias', outputSomething);
+
+ function getData(dataURI, callback) {
+    console.log('første linje i getData funktionen...');
+    var timer = setTimeout(function () {
+       console.log('Her der der gået 2 sekunder.');
+       var dataArray = [12, 34, 56, 78, 90, 123, 456, 789];
+       callback(dataArray);
+    }, 2000);
+ }
+ 
+ function outputData(myData) {
+    console.log('Dette er funktionen der udskriver det hentede data: ' + myData);
+ }
+ 
+ getData('http://www.domain.com/something', outputData);
+ console.log("Denne kode linje burde være den sidste.");
